0f2fcbac518eaae10f358fd402f399c096b6beef
Kevin O'Neil <kevinmoneil@me.com>
2017-12-19 20:11:55 +0900

    the start

--- examples/app.js
+++ examples/app.js
@@ -4,8 +4,6 @@ import Basic from './basic';
 import Text from './text';
 import Robot1 from './robot1';
 
-console.log(Text);
-
 ReactDOM.render(
   <React.Fragment>
     <Basic />
--- src/SVGLoader.js
+++ src/SVGLoader.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import fetch from 'unfetch';
 import PropTypes from 'prop-types';
-import ReactSVG from 'react-svg';
+import ReactDOMServer from 'react-dom/server';
+
+const isBrowser = typeof window !== 'undefined';
+const SvgInjector = isBrowser ? require('svg-injector') : null;
 
 /* Just a wrapper around ReactSVG to disable re rendering it */
 class SVGLoader extends React.Component {
@@ -11,18 +15,32 @@ class SVGLoader extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      el: ''
+    };
+  }
+
+  componentDidMount() {
+    const svgEl = document.createElement('svg');
+    const elStr = ReactDOMServer.renderToStaticMarkup(
+      <img src={this.props.path} className="inject-me" />
+    );
+    svgEl.innerHTML = elStr;
+    const injectEl = document.querySelector('.inject-me');
   }
 
   shouldComponentUpdate(nextProps) {
     return nextProps.path !== this.props.path;
   }
+
   render() {
     return (
-      <ReactSVG
+      <svg
         className={this.props.className}
+        ref={this.onSVGReady}
         style={this.props.style}
         path={this.props.path}
-        callback={this.props.onSVGReady}
+        dangerouslySetInnerHTML={{ __html: this.state.el }}
       />
     );
   }
